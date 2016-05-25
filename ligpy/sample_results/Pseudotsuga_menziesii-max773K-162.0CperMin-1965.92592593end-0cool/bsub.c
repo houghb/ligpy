6b77d9de77d323aaf632570819dd4abec3b91a3e
houghb <houghb@uw.edu>
2016-05-25 13:21:54 -0700

    add sample results for douglas fir

---
+++ ligpy/sample_results/Pseudotsuga_menziesii-max773K-162.0CperMin-1965.92592593end-0cool/bsub.c
@@ -0,0 +1,8 @@
+#include <parest.h>
+extern InputStructure *gInputPtr;
+extern const double Renergy, Rgas;
+
+void bsub_(double *t, int *nstvar, double u[], double fpj[], int *jspar,
+	       double rpar[], int ipar[], int *ieform, int *ires)
+{
+}
